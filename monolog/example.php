293a8282771b1578d56c25ae95c4217901a5470b
Artur Moczulski <artur.moczulski@gmail.com>
2017-11-01 02:09:54 +0000

    github-rollbarphp-275: monolog example based on rollbar-php issue 275

--- monolog/example.php
+++ monolog/example.php
@@ -17,3 +17,18 @@ Rollbar::init($config);
 
 $logger = new Logger('ExampleMonologLogger');
 $logger->pushHandler(new PsrHandler(Rollbar::logger()));
+$example = new \ExampleVendor\ExampleClass();
+
+// catch the exception; Rollbar will NOT perform the default handling
+try {
+    
+    $example->throwExampleException();
+    
+} catch (\Exception $exception) {
+    
+    $logger->error($exception);
+    
+}
+
+// don't catch the exception; let Rollbar perform the dafault handling
+$example->throwExampleException();
---
+++ monolog/src/ExampleClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ExampleVendor;
+
+class ExampleClass
+{
+    
+    public function throwExampleException() {
+        
+        throw new \Exception("Example exception");
+        
+    }
+    
+}
