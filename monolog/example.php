e8b4366ff107d22f689e6f4bf38e737b7552b719
Artur Moczulski <artur.moczulski@gmail.com>
2017-11-02 03:10:06 +0000

    github-2: clean up the monolog example

--- monolog/example.php
+++ monolog/example.php
@@ -17,18 +17,4 @@ Rollbar::init($config);
 
 $logger = new Logger('ExampleMonologLogger');
 $logger->pushHandler(new PsrHandler(Rollbar::logger()));
-$example = new \ExampleVendor\ExampleClass();
-
-// catch the exception; Rollbar will NOT perform the default handling
-try {
-    
-    $example->throwExampleException();
-    
-} catch (\Exception $exception) {
-    
-    $logger->error($exception);
-    
-}
-
-// don't catch the exception; let Rollbar perform the dafault handling
-$example->throwExampleException();
+$logger->error(new \Exception("Example exception logged by Monolog."));
